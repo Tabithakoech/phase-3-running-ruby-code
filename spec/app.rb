02c9897155d9c3212c948f9d190778dfab8b0d07
TabithaKoech <tabbyk1718@gmail.com>
2022-08-19 22:04:48 +0300

    commit tree 6e0e3857ea9fbf79f4cba7f1649587ead4d8b3b1

---
+++ spec/app.rb
@@ -0,0 +1,9 @@
+puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# puts [1, 2, 3]
+# p [1, 2,
